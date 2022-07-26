260df4af80426b00e9e535efb2b9d325e1cfaa6b
Benz0X <nils.exibard@gmail.com>
2022-07-26 19:04:43 +0200

    Switch to hall effect sensor gathered by an MCP23X17 Add another MIDI out (to RPI) Cleanup code

--- src/main.cpp
+++ src/main.cpp
@@ -14,18 +14,28 @@
 #include <SFE_BMP180.h>
 #include <Wire.h>
 #include <list>
-
+#include <Adafruit_MCP23X17.h>
+#include <Adafruit_BMP085.h>
 //Matrix definition
 #define ROW_NUMBER_R 11
 #define COLUMN_NUMBER_R 3
 
-//Useful flags
-#define DEBUG   1
-#define OCTAVED 0
+//Useful define
+#define PUSH 1
+#define NOPUSH 2
+#define PULL 0
 
-//Matrix-->arduino pin correspondance
-uint8_t rx_pin[] = {28, 26, 24};
-uint8_t ry_pin[] = {25, 27, 29, 31, 35, 37, 39, 41, 43, 45, 47};
+//Useful flags
+#define DEBUG   0
+#define OCTAVED 1
+
+#define MIDI_GPIO
+
+
+//Contains pressed keys
+uint32_t keys_md;
+uint16_t keys_md_row[3];
+
 
 //Contain the information 'is button pressed ?' for whole matrix
 bool R_press[COLUMN_NUMBER_R][ROW_NUMBER_R];
@@ -58,63 +68,116 @@ uint8_t R_notesP[COLUMN_NUMBER_R][ROW_NUMBER_R] = {
 std::list<int> notes_to_play;
 std::list<int> notes_to_remove;
 
-//Pressure stuff
-int volume;
-int state, state_2; //Contains info if current bellow direction is push or pull
-int prev_state;
-SFE_BMP180 pressure;
+//MCP23X17
+Adafruit_MCP23X17 mcp_md_0;
+Adafruit_MCP23X17 mcp_md_1;
+
+//pressure stuff
+uint8_t volume, volume_prev;
+uint8_t bellow, bellow_not_null, bellow_prev; //Contains info if current bellow direction is push or pull
+SFE_BMP180 bmp_in;
 double p_tare;
 double p_offset;
+double T, P;
 long t_start;
 float min_pressure = 0.2;
 float max_pressure = 15;
+char status;
+
+//Debug stuff
+byte data[5];
+char buffer[256];
+uint32_t buffpointer;
+
 
 //Midi creation
-MIDI_CREATE_INSTANCE(HardwareSerial, Serial, MIDI);
+MIDI_CREATE_INSTANCE(HardwareSerial, Serial, MIDIUSB);
+MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDIPI);
 
 //Initial setup
 void setup()
 {
-    //pressure stuff
-    prev_state = 1;
-    p_tare = 0;
-    if (pressure.begin()) {}
-    else
-    {
+
+    //Init both MIDI
+    MIDIUSB.begin(1);
+    MIDIPI.begin(1);
+    Serial.begin(115200);
+    Serial1.begin(115200);
+
+    //Set I2C frequ
+    Wire.begin();  
+    Wire.setClock(400000);
+
+    //Init pressure sensor
+    if (DEBUG) {Serial.println("Init BMP180\n");}
+     if (!bmp_in.begin()) {
         Serial.println("BMP180 init fail\n\n");
         while (1); // Pause forever.
     }
 
-    //midi and IO
-    MIDI.begin(1);
-    Serial.begin(115200);
-    if (DEBUG) {SerialUSB.println("Initializing...");}
-    for (size_t i = 0; i < COLUMN_NUMBER_R; i++) {
-        pinMode(rx_pin[i], OUTPUT);
-        digitalWrite(rx_pin[i], HIGH);
+    //Init MCP
+    if (DEBUG) {Serial.println("Init mcp_md_0\n");}
+    if (!mcp_md_0.begin_I2C()) {
+        Serial.println("Error mcp_md_0.");
+        while (1);
     }
-    for (size_t i = 0; i < ROW_NUMBER_R; i++) {
-        pinMode(ry_pin[i], INPUT);
-        digitalWrite(ry_pin[i], HIGH);
+    if (DEBUG) {Serial.println("Init mcp_md_1\n");}
+    if (!mcp_md_1.begin_I2C(0x24)) {
+        Serial.println("Error mcp_md_1.");
+        while (1);
     }
+
+
+    //Configure MCPs to all input PULLUP
+    for(int i=0;i<16;i++){
+        mcp_md_0.pinMode(i, INPUT_PULLUP);
+        mcp_md_1.pinMode(i, INPUT_PULLUP);
+    }
+
+    //init some variable
     memset(R_prev_press, 0, sizeof(R_prev_press));
-    if (DEBUG) {SerialUSB.println("End INIT...");}
+    volume_prev=0;
+    bellow_prev=NOPUSH;
+
+    //Mesure initial pressure at boot
+    p_tare=0;
+    status = bmp_in.startTemperature();
+    if (status == 0) {Serial.println("error retrieving pressure measurement\n");}
+    delay(status);
+    status = bmp_in.getTemperature(T);
+    if (status == 0) {Serial.println("error retrieving pressure measurement\n");}
+
+    // Start a pressure measurement:
+    status = bmp_in.startPressure(3);
+    if (status == 0) {Serial.println("error retrieving pressure measurement\n");}
+
+    t_start = millis();
+    while (millis() - t_start < status);
+    status = bmp_in.getPressure(P, T);
+    if (status == 0) {Serial.println("error retrieving pressure measurement\n");}
+    p_tare=P;
+
+
+    data[0]=0xf0;
+    data[1]=0x7d;
+    data[2]=0x03;
+    data[3]=0x01;
+    data[4]=0xf7;
+        MIDIPI.sendControlChange(7,70, 1);
+
+        MIDIPI.sendSysEx(5, data, 1);
+
+    if (DEBUG) {Serial.println("INIT OK.");}
 }
 
 //Main loop
 void loop()
 {
-    //-----------------------------------
-    // local variables
-    //-----------------------------------
-    //Temperature and Pressure
-    double T, P;
-    //Status for the pressure sensor
-    char status;
 
     //-----------------------------------
     // Initialise loop
     //-----------------------------------
+    buffpointer=0;
     //remember old pressed touch
     memcpy(R_prev_press, R_press, sizeof(R_press));
     memset(R_press, 0, sizeof(R_press));
@@ -123,77 +186,82 @@ void loop()
     // Temp & pressure mesurement part 1
     //-----------------------------------
     // Start temp mesurement
-    status = pressure.startTemperature();
+    status = bmp_in.startTemperature();
     if (status == 0) {Serial.println("error retrieving pressure measurement\n");}
     // Wait for the measurement to complete:
     delay(status);
 
     // Retrieve the completed temperature measurement:
-    status = pressure.getTemperature(T);
+    status = bmp_in.getTemperature(T);
     if (status == 0) {Serial.println("error retrieving pressure measurement\n");}
 
     // Start a pressure measurement:
-    status = pressure.startPressure(3);
+    status = bmp_in.startPressure(3);
     if (status == 0) {Serial.println("error retrieving pressure measurement\n");}
 
     t_start = millis();
 
+
     //-----------------------------------
     // Matrix key acquisition
     //-----------------------------------
-    for (size_t i = 0; i < COLUMN_NUMBER_R; i++) {
-        digitalWrite(rx_pin[i], LOW);
-        for (size_t j = 0; j < ROW_NUMBER_R; j++)
-        {
-            if (digitalRead(ry_pin[j]) == LOW) {R_press[i][j] = 0;}
-            else {R_press[i][j] = 1;}
-        }
-        digitalWrite(rx_pin[i], HIGH);
-    }
+    keys_md = (uint32_t) mcp_md_0.readGPIO(1) & 0xff;
+    keys_md |= ((uint32_t) mcp_md_0.readGPIO(0) & 0xff)<<8;
+    keys_md |= ((uint32_t) mcp_md_1.readGPIO(1) & 0xff)<<16;
+    keys_md |= ((uint32_t) mcp_md_1.readGPIO(0) & 0xff)<<24;
 
+    //Conversion to R_press
+    keys_md_row[0] = keys_md >> (10+ROW_NUMBER_R);
+    keys_md_row[1] = (keys_md >> 10) & 0x7FF;
+    keys_md_row[2] = keys_md & 0x3FF;
+    for(uint8_t i=0;i<COLUMN_NUMBER_R;i++){
+        for(uint8_t j=0;j<ROW_NUMBER_R;j++){
+            R_press[i][j]=(keys_md_row[i] & (1<<(j)))>>(j);
+        }
+    }
 
     //-----------------------------------
     // Pressure mesurement part 2 & calc
     //-----------------------------------
     while (millis() - t_start < status);
-    status = pressure.getPressure(P, T);
+    status = bmp_in.getPressure(P, T);
     if (status == 0) {Serial.println("error retrieving pressure measurement\n");}
 
     //Get the pressure at initial time (if not defined)
-    if (p_tare == 0) {p_tare = P;}
     p_offset = P - p_tare;
     //Trunk the pressure if too big/low
     if (p_offset > max_pressure) {p_offset = max_pressure;}
     if (p_offset < -max_pressure) {p_offset = -max_pressure;}
     //If no pressure, no volume
     if (abs(p_offset) < min_pressure) {
-        state = 0;
+        bellow = NOPUSH;
         volume = 0;
     } else {
         if (p_offset < 0) { //PULL
-            state = -1;
-            state_2 = -1;
+            bellow          = PULL;
+            bellow_not_null = PULL;
         } else {            //PUSH
-            state = 1;
-            state_2 = 1;
+            bellow          = PUSH;
+            bellow_not_null = PUSH;
         }
         //volume calculation :
         p_offset = abs(p_offset);
-        volume = int((log(float(p_offset) / 55) + 6) * 27) ;
+        volume = uint8_t((log(float(p_offset) / 55) + 6) * 27) ;
     }
 
 
     //-----------------------------------
     // Prepare MIDI message
     //-----------------------------------
-    for (size_t i = 0; i < COLUMN_NUMBER_R; i++) {
+   for (size_t i = 0; i < COLUMN_NUMBER_R; i++) {
         for (size_t j = 0; j < ROW_NUMBER_R; j++) {
-            if (prev_state == 1 && state == -1) {
+            //Add and remove note depending on bellow direction and previous direction
+            if (bellow_prev == PUSH && bellow == PULL) {
                 if (R_press[i][j]) {
                     notes_to_remove.push_back(R_notesP[i][j]);
                     notes_to_play.push_back(R_notesT[i][j]);
                 }
-            } else if (prev_state == -1 && state == 1) {
+            } else if (bellow_prev == PULL && bellow == PUSH) {
                 if (R_press[i][j]) {
                     notes_to_remove.push_back(R_notesT[i][j]);
                     notes_to_play.push_back(R_notesP[i][j]);
@@ -201,18 +269,18 @@ void loop()
             }
             if (R_prev_press[i][j]) {
                 if (!R_press[i][j]) { //remove note if touch isn't pressed anymore
-                    if (state_2 == 1 || prev_state == 1)
+                    if (bellow_not_null == PUSH || bellow_prev == PUSH)
                     {
                         notes_to_remove.push_back(R_notesP[i][j]);
                     }
-                    if (state_2 == -1 || prev_state == -1)
+                    if (bellow_not_null == PULL || bellow_prev == PULL)
                     {
                         notes_to_remove.push_back(R_notesT[i][j]);
                     }
                 }
-            } else if (!R_prev_press[i][j]) {
+            } else if (!R_prev_press[i][j]) { //Add notes
                 if (R_press[i][j]) {
-                    if (state_2 == 1) {
+                    if (bellow_not_null == PUSH) {
                         notes_to_play.push_back(R_notesP[i][j]);
                     }
                     else {
@@ -232,7 +300,8 @@ void loop()
         //Serial.println(notes_to_remove.back(),10);
         if (!DEBUG)
         {
-            MIDI.sendNoteOff(notes_to_remove.back()+12*OCTAVED, 0, 1);
+            MIDIUSB.sendNoteOff(notes_to_remove.back()+12*OCTAVED, 0, 1);
+            MIDIPI.sendNoteOff(notes_to_remove.back()+12*OCTAVED, 0, 1);
         }
 
         notes_to_remove.pop_back();
@@ -243,41 +312,42 @@ void loop()
         //Serial.println(notes_to_play.back(),10);
         if (!DEBUG)
         {
-        MIDI.sendNoteOn(notes_to_play.back()+12*OCTAVED, 100, 1);
+        MIDIUSB.sendNoteOn(notes_to_play.back()+12*OCTAVED, 100, 1);
+        MIDIPI.sendNoteOn(notes_to_play.back()+12*OCTAVED, 100, 1);
         }
         notes_to_play.pop_back();
     }
-    if (state != 0) {prev_state = state;}
+    if (bellow != NOPUSH) {bellow_prev = bellow;}
 
-  //delay(500);
   
     if (!DEBUG)
     {
-        MIDI.sendControlChange(7,volume, 1);
-        // MIDI.sendControlChange(7,100, 1);
+        if(volume_prev!=volume){
+            MIDIUSB.sendControlChange(7,volume, 1);
+            MIDIPI.sendControlChange(7,volume, 1);
+        }
     }
+    volume_prev=volume;
 
 
-    //-----------------------------------
-    // DEBUG
-    //-----------------------------------
-    //If debug, print the current detected pressed touch on terminal with X, other with O
     if (DEBUG)
-    {
-        Serial.write(27);       // ESC command
-        Serial.print("[2J");    // clear screen command
-        Serial.write(27);
-        Serial.print("[H");     // cursor to home command
-        for (size_t j = 0; j < ROW_NUMBER_R; j++) {
-            for (size_t i = 0; i < COLUMN_NUMBER_R; i++) {
-                if(R_press[i][j]) {
-                    Serial.print("X");
-                } else {
-                    Serial.print("O");
+        {
+            // Serial.write(27);       // ESC command
+            // Serial.print("[2J");    // clear screen command
+            // Serial.write(27);
+            // Serial.print("[H");     // cursor to home command
+            for (size_t j = 0; j < COLUMN_NUMBER_R; j++) {
+                for (size_t i = 0; i < ROW_NUMBER_R; i++) {
+                    if(R_press[j][i]) {
+                        Serial.print("X");
+                    } else {
+                        Serial.print("-");
+                    }
                 }
+                Serial.print("   ");
             }
             Serial.print("\n\r");
+            delay(100);
         }
-        delay(100);
-    }
+
 }
